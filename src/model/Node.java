4738c6ea89575bdb537c60793ad195aa6d4685f6
Vinicius <ivmmc99@gmail.com>
2025-08-14 11:33:50 -0300

    Update Node.java

--- src/model/Node.java
+++ src/model/Node.java
@@ -2,7 +2,19 @@ package model;
 
 import java.util.Collection;
 
-public class Node implements INode {
+class Node implements INode {
   public String logadouro;
   public lat-long coordinates;
+
+  public Node(String logadouro, lat-long coordinates){
+    this.logadouro = logadouro;
+    this.coordinates = coordinates;
+  }
+
+  public String getLogadouro(){
+    return logadouro;
+  }
+  public lat-long GetCoordinates(){
+    return coordinates;
+  }
 }
