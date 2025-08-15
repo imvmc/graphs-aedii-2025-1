86b58144978b4dcff2125a95241817fa7bd6ff6c
Vinicius <ivmmc99@gmail.com>
2025-08-15 11:41:12 -0300

    Update Edge.java

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -3,5 +3,33 @@ package model;
 import java.util.Collection;
 
 public class Edge implements IEdge {
-  Node from;
-  Node to;
+  private Node from;
+  private Node to;
+  private String idLogradouro;
+  private int weightTime;
+  private double distance;
+  private AreaType areaType;
+
+  public Edge(Node from, Node to, double distance, String idLogradouro, int pesoTempo, AreaType areaType) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+        this.idLogradouro = idLogradouro;
+        this.pesoTempo = pesoTempo;
+        this.areaType = areaType;
+    }
+
+  @Override
+    public double calculoPeso() {
+        return distance;
+    }
+
+    @Override
+    public double getTempo() {
+        return pesoTempo;
+    }
+
+    public Node getFrom() { return from; }
+    public Node getTo() { return to; }
+}
+  
