33ac452af11d7b041cdb0a093535014b84643ee4
Vinicius <ivmmc99@gmail.com>
2025-08-17 22:04:37 -0300

    Create TimeCalculator.java

---
+++ src/calculation/TimeCalculator.java
@@ -0,0 +1,9 @@
+package calculation;
+
+public class TimeCalculator {
+  public static double calculate(Edge e, double kmSpeed) {
+        double distanceKm = e.getDistance();
+        double hourTime = distanceKm / kmSpeed;
+        return hourTime * 3600.0;
+    }
+}
