ea9b128afc896a7f9d563638334ed17b77880f03
Vinicius <ivmmc99@gmail.com>
2025-08-17 21:57:42 -0300

    Update IEdge.java

--- src/entities/IEdge.java
+++ src/entities/IEdge.java
@@ -3,16 +3,5 @@ import java.lang.reflect.Type;
 import java.util.Collection;
 
 public interface IEdge {
-  public Node from;
-  public Node to;
-  public double distance;
-  public boolean isDoubleVia;
-  public Density density;
-
-  public double CalculoPeso(){
-  }
-
-  public double GetTempo(){
-  }
-}
   
+}
